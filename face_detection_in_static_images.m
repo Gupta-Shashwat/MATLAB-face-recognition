4d3a42bc74e61e6ee12978a60a7f8fc2d96d7691
theHTMLprogrammer <stgupta9935@gmail.com>
2023-04-17 00:13:16 +0530

    Added image recognition feature in static images

--- face_detection_in_static_images.m
+++ face_detection_in_static_images.m
@@ -4,10 +4,7 @@ img=imread(imgPath);
 grayImg=rgb2gray(img);
 
 % Detecting facial features
-faceDetector=vision.CascadeObjectDetector('Mouth','MergeThreshold',100);
-noseDetector=vision.CascadeObjectDetector('Nose','MergeThreshold',50);
+faceDetector=vision.CascadeObjectDetector();
 mouthBBOXES=step(faceDetector, grayImg);
-noseBBOXES=step(noseDetector, grayImg);
-faces=insertObjectAnnotation(img,"rectangle",mouthBBOXES,"Mouth");
-nose=insertObjectAnnotation(img, "rectangle", noseBBOXES, "Nose");
-imshow(nose);
+face=insertObjectAnnotation(img,"rectangle",mouthBBOXES,"Face");
+imshow(face);
